719f889840cf3e4bc97b66b476e026e77686b700
Abey Onalaja <abey.onalaja@gmail.com>
2018-07-17 22:46:55 +0100

    Server set up and render hello world template

---
+++ template.js
@@ -0,0 +1,12 @@
+export default() => {
+    return `<!doctype html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title>MERN</title>
+        </head>
+        <body>
+            <div id="root">Hello World</div>
+        </body>
+    </html>`
+}
